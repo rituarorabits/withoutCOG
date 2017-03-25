c5dc441dce56f4c2d83e36d22ed5d79389b8ed64
deeksha004 <deekshaaggarwal@gmail.com>
2017-03-25 12:52:02 +0530

    Second commit by megha

--- SnakeAndLadder/src/gameBoard/initializeBoard.java
+++ SnakeAndLadder/src/gameBoard/initializeBoard.java
@@ -2,5 +2,17 @@ package gameBoard;
 
 public class initializeBoard {
 
+	boolean checkValidity(Snake s)
+	{
+		if(s.startPosition>s.endPosition)
+		{return true;}
+		else
+		{return false;}
+	}
+	
+	void initSnakes(Snake s)
+	{
+		Snake a= new Snake(12,23,2);
+	}
 	
 }
