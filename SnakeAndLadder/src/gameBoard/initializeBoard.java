f4d30194608318568f77de635d2ba8a2bfa61ccd
DravitLochan <dlochangupta@gmail.com>
2017-03-25 12:42:55 +0530

    dev A activity

--- SnakeAndLadder/src/gameBoard/initializeBoard.java
+++ SnakeAndLadder/src/gameBoard/initializeBoard.java
@@ -2,5 +2,16 @@ package gameBoard;
 
 public class initializeBoard {
 
+	public boolean checkValidity(Ladder l)
+	{
+		if(l.startPosition<l.endPosition)
+			return true;
+		return false;
+	}
+	
+	public void initLadders(Ladder l)
+	{
+		boolean isValid = checkValidity(l);
+	}
 	
 }
